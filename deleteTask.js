54c88260427a4a8d446d9860da1dcde2af9a25eb
Aakash-Krishnan <mrsks156@gmail.com>
2024-05-20 21:56:17 +0530

    Updating with delete tasks validations

--- deleteTask.js
+++ deleteTask.js
@@ -8,7 +8,12 @@ window.addEventListener('dblclick', e => {
   const boardEl = parent.parentElement.parentElement
 
   if (parent.className === 'task') {
-    alterBoardData(parent, boardEl) // #Goto ./helper.js line --> 40
-    parent.remove()
+    let confirmation = window.confirm(
+      `Are you sure you want to delete this task --> ${parent.children[0].innerText}`
+    )
+    if (confirmation) {
+      alterBoardData(parent, boardEl) // #Goto ./helper.js line --> 40
+      parent.remove()
+    }
   }
 })
