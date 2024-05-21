8da3a3793ff52a11fddb9e2a6c7957b14e929073
Aakash-Krishnan <mrsks156@gmail.com>
2024-05-21 13:01:32 +0530

    minor changes

--- instructions.js
+++ instructions.js
@@ -1,6 +1,12 @@
 const instBtn = document.querySelector('.inst-btn')
 const instModal = document.querySelector('.inst-modal')
 
+document.addEventListener('keydown', e => {
+  if (e.altKey && e.key === 'i') {
+    instBtn.click()
+  }
+})
+
 instBtn.addEventListener('click', e => {
   if (instModal.style.display === 'block') {
     instModal.style.display = 'none'
