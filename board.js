d8fa6bd562e210b142dae55b497c486de31736f4
lokappakishan <lokappakishan@gmail.com>
2024-05-23 17:37:20 +0200

    added functionlity to ad items in any order

--- board.js
+++ board.js
@@ -2,10 +2,32 @@
 
 const boards = document.querySelectorAll('.board');
 
-boards.forEach((board)=>{
-    board.addEventListener('dragover',function(){
+boards.forEach(board => {
+    board.addEventListener('dragover', function(event) {
+        event.preventDefault();
         const draggedItem = document.querySelector('.is-dragging');
-        board.appendChild(draggedItem);
+        const boardItems = [...board.querySelectorAll('.item:not(.is-dragging)')];
+        const mouseY = event.clientY;
+
+        let closestItem = null;
+        let closestItemOffset = Number.NEGATIVE_INFINITY;
+
+        boardItems.forEach(item => {
+            const box = item.getBoundingClientRect();
+            const offset = mouseY - box.top - (box.height / 2);
+
+            if (offset < 0 && offset > closestItemOffset) {
+                closestItemOffset = offset;
+                closestItem = item;
+            }
+        });
+
+        if (closestItem) {
+            board.insertBefore(draggedItem, closestItem);
+        } else {
+            board.appendChild(draggedItem);
+        }
+
         saveToLocalStorage();
-    })
-})
+    });
+});
